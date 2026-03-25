5368ec37d860cf36aba996503547712a4847374e
Alexth-pope67 <736521@student.dusd.net>
2026-03-25 13:49:21 -0700

    did 3.2
    MaDE obstaclesd

--- U3SoundAndEffectsP6Daboul/Assets/Scripts/PlayerController.cs
+++ U3SoundAndEffectsP6Daboul/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,40 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
-
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
         }
     }
 }
